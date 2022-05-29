ab25ff5ce8991267f6a3615e153bca9955fb3ddc
Rafaela Tillmann <rafaelatillmann.rt@gmail.com>
2022-05-28 23:14:37 -0300

    base motorista

---
+++ model/usuario.py
@@ -0,0 +1,7 @@
+class Usuario:
+    def __init__(self, name: str, email: str, cpf: int, senha: str):
+        self.__id = None
+        self.__nome = nome
+        self.__email = email
+        self.__cpf = cpf
+        self.__senha = senha
