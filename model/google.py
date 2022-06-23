9fd7fca6f07612aa31220cb99abb2072840838fe
Rafaela Tillmann <rafaelatillmann.rt@gmail.com>
2022-06-23 19:21:06 -0300

    inicio google api

---
+++ model/google.py
@@ -0,0 +1,8 @@
+class Google:
+    def __init__(self):
+        self.__base_url = '',
+        self.__matrix_url = '/maps/api/distancematrix/json'
+        self.__key = ''
+
+    def request(self):
+        pass
