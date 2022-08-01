94f12d2cbacaeab046aec5e0be7b906d950dabfd
Roger <rogerbambinetti@hotmail.com>
2022-08-01 19:00:04 -0300

    controller

--- controller/controllerIncidente.py
+++ controller/controllerIncidente.py
@@ -1,5 +1,6 @@
 from dao.daoIncidente import DaoIncidente
 from view.viewIncidente import ViewIncidente
+from dao.daoRota import DaoRota
 
 from model.incidente import Incidente
 
@@ -11,6 +12,7 @@ class ControllerIncidente():
     def __init__(self, session):
         self.__dao_incidente = DaoIncidente
         self.__view = ViewIncidente()
+        self.__dao_rota = DaoRota
         self.__session = session
 
     def options(self, route):
@@ -18,6 +20,11 @@ class ControllerIncidente():
             button, values = self.__view.options()
             if not self.__session.menu(button):
                 if button == 'save':
+                    if values['tipo'] == 'grave':
+                        print('GRAVE')
+                        route.fim = datetime.datetime.now()
+                        self.__dao_rota.update(route)
+
                     incidente = Incidente(datetime.datetime.now() ,values['descricao'], route, values['tipo'])
                     self.__dao_incidente.insert(incidente)
                     break
